91032194e3d338ecb695ccc7a77b63fdf033e809
Melvin Lee <melvin.lee@iswish.nl>
2021-02-05 18:13:33 +0100

    feature: custom rule + rule specifications

---
+++ features/schema-names-pascal-case.feature
@@ -0,0 +1,34 @@
+Feature: Schema names Must be written in PascalCase
+
+  Background:
+    Given the Spectral rule 'schema-names-pascal-case'
+
+  Scenario: Schema name is written in PascalCase
+    Given the OAS3 description document
+    """
+    openapi: 3.0.3
+    components:
+      schemas:
+        PascalCaseComponent:
+          type: object
+    """
+    When the OAS description document is linted
+    Then there should be no errors
+
+  Scenario Outline: Schemas name is not written in PascalCase
+    Given the OAS3 description document
+    """
+    openapi: 3.0.3
+    components:
+      schemas:
+        <component name>:
+          type: object
+    """
+    When the OAS description document is linted
+    Then the error message should be 'component \'<component name>\' must be pascal case'
+
+    Examples:
+      | component name       |
+      | camelCaseComponent   |
+      | snake_case_component |
+      | kebab-case-component |
---
+++ features/step_definitions/stepdefs.js
@@ -0,0 +1,31 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+const { Spectral, Document, Parsers, isOpenApiv3 } = require('@stoplight/spectral');
+const { join } = require('path');
+const should = require('chai').should();
+
+Given('the Spectral rule {string}', function(rule) {
+    this.rule = rule;
+});
+
+Given('the OAS3 description document', function(oas3Document) {
+    this.document = new Document(oas3Document, Parsers.Yaml);
+});
+
+When('the OAS description document is linted', async function() {
+    const spectral = new Spectral();
+    spectral.registerFormat('oas3', isOpenApiv3);
+    await spectral.loadRuleset([
+        join(__dirname, '../../spectral_rules/' + this.rule + '.yaml')
+    ]);
+    this.results = await spectral.run(this.document);
+});
+
+Then('there should be no errors', function() {
+    this.results.should.have.lengthOf(0, JSON.stringify(this.results, null, "\t"));
+});
+
+Then('the error message should be {string}', function(expected) {
+    this.results.should.have.lengthOf(1, JSON.stringify(this.results, null, "\t"));
+    this.results[0].code.should.equal(this.rule, JSON.stringify(this.results, null, "\t"));
+    this.results[0].message.should.equal(expected, JSON.stringify(this.results, null, "\t"));
+});
